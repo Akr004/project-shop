9c6dfe75440ff90e5bd8e4f7547b1ebbf3640e21
amit <amitrao06@gmail.com>
2025-08-12 21:31:19 +0530

    add image controller post image  implementation

--- shoping-app/src/main/java/com/shoping/app/controller/ImageController.java
+++ shoping-app/src/main/java/com/shoping/app/controller/ImageController.java
@@ -1,18 +1,20 @@
 package com.shoping.app.controller;
 
 import com.shoping.app.dto.ImageDto;
+import com.shoping.app.exception.ResourceNotFoundException;
 import com.shoping.app.model.Image;
 import com.shoping.app.response.ApiResponse;
 import com.shoping.app.service.image.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -22,12 +24,36 @@ public class ImageController {
     private final ImageService imageService;
 
     @PostMapping("/upload")
-    public ResponseEntity<ApiResponse> saveImages(@RequestParam List<MultipartFile> files,@RequestParam Long productId){
+    public ResponseEntity<ApiResponse> saveImages(@RequestBody List<MultipartFile> files,@RequestParam Long productId){
         try {
             List<ImageDto> imageDtos = imageService.saveImage(files, productId);
             return ResponseEntity.ok(new ApiResponse("upload success",imageDtos));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("upload failed",e.getMessage()));
         }
+    }
+
+    @GetMapping("/images/download/{imageId}")
+    public ResponseEntity<ByteArrayResource> downloadImage(@PathVariable Long imageId) throws SQLException {
+        Image image = imageService.getImageById(imageId);
+        ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1, (int) image.getImage().length()));
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; fileName=\"" +image.getFileName() +"\"")
+                .body(resource);
+    }
+
+    @PutMapping("/image/{imageId}")
+    public ResponseEntity<ApiResponse> updateImage(@PathVariable Long imageId,@RequestBody MultipartFile file){
+        try {
+            Image image = imageService.getImageById(imageId);
+            if (image != null){
+                Image updateImage = imageService.updateImage(file, imageId);
+                return ResponseEntity.ok(new ApiResponse("update Success !",updateImage));
+            }
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("update failed !",HttpStatus.INTERNAL_SERVER_ERROR));
+
     }
 }
